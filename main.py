7d51007164cdb7df58a884b6051982486689bc8c
Near <szewei2004@gmail.com>
2024-03-30 00:13:27 +0800

    Added typewritter effect

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 import json
+import time
 from difflib import get_close_matches
 
 # Settings
 knowledgebase_folder = "Knowledgebase"
 knowledgebase_file = "Iris.json"
+
+bot_prompt = "Iris: "
+user_prompt = ">> "
 default_notfound_error = "I don't know how to respond. Please teach me or type [SKIP]."
 default_iris_thank = "Thank you! I learnt a new thing."
 
@@ -40,29 +44,41 @@ def get_response_for_prompt(prompt: str, knowledge_base: dict) -> str | None:
             return p["response"]
 
 
+# Typewritter Effect
+def typewrite(prefix, text, delay=0.05):
+    print(prefix, end='', flush=True)  # Print the prefix normally
+    for char in text:
+        print(char, end='', flush=True)  # Print each character of the text with the typewriter effect
+        time.sleep(delay)
+    print()  # Add a newline after printing the text
+
+
 # Main Function
 def main():
     knowledge_base: dict = load_knowledge_base(file_path)
 
     while True:
-        user_input: str = input(">>")
+        user_input: str = input(user_prompt)
 
-        if user_input.lower() == "quit":
+        if user_input.lower() == "q":
             break
+
+        if user_input.lower() == "cls":
+            os.system("cls")
 
         best_match: str | None = find_best_match(user_input, [p["prompt"] for p in knowledge_base["prompts"]])
 
         if best_match:
             response: str = get_response_for_prompt(best_match, knowledge_base)
-            print(f"Iris: {response}")
+            typewrite(bot_prompt, response)
         else:
-            print(f"Iris: {default_notfound_error}")
-            new_response: str = input(">>")
+            print(bot_prompt, default_notfound_error)
+            new_response: str = input(user_prompt)
 
             if new_response.lower() != "skip":
                 knowledge_base["prompts"].append({"prompt": user_input, "response": new_response})
                 save_knowledge_base(file_path, knowledge_base)
-                print(f"Iris: {default_iris_thank}")
+                typewrite(bot_prompt, default_iris_thank)
 
 
 # Run Script
